753918d30245ed4be4deb1b61277bc848d980178
Cristiano Joaquim dos Santos <cristiano.joaquim@gmail.com>
2018-05-25 00:05:08 -0300

    Atualização do Script da Carga de Dimensão de Tempo

--- scripts/Carga Dimensão Tempo.sql
+++ scripts/Carga Dimensão Tempo.sql
@@ -1,16 +1,17 @@
 USE DMVendas
 GO
 
-DECLARE @DataIni DATE = '2016-01-01' 
+DECLARE @DataIni DATE = '2016-01-01'
 DECLARE @DataFim DATE = '2019-12-31'
 
 DECLARE @DataCurrent DATE
 
 DECLARE @cont INT = 0
+DECLARE @SKTempo INT = 0
 DECLARE @Data DATE
-DECLARE @Ano INT = 0
-DECLARE @Mes INT = 0
 DECLARE @Dia INT = 0
+DECLARE @Mes INT = 0
+DECLARE @Ano INT = 0
 
 BEGIN
 	SET @DataCurrent = @DataIni
@@ -18,16 +19,13 @@ BEGIN
 	WHILE @DataCurrent <= @DataFim
 	BEGIN
 		SET @Data = @DataCurrent
-		SET @Ano = YEAR(@DataCurrent)
-		SET @Mes = MONTH(@DataCurrent)
-		SET @Dia = DAY(@DataCurrent)
+		SET @SKTempo = CONVERT(INT, REPLACE(@Data, '-', ''))
+		SET @Ano = YEAR(@Data)
+		SET @Mes = MONTH(@Data)
+		SET @Dia = DAY(@Data)
 
 		INSERT INTO dbo.DimTempo (SKTempo, Data, Dia, Mes, Ano)
-		VALUES (CONVERT(INT, REPLACE(@Data, '-', '')),
-				@Data,
-				@Dia,
-				@Mes,
-				@Ano)
+		VALUES (@SKTempo, @Data, @Dia, @Mes, @Ano)
 
 		SET @cont += 1
 		SET @DataCurrent = DATEADD(DAY, @cont, @DataIni)
