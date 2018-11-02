def4cad6a70600844c4cf2b329af46e9755b425c
Cristiano Joaquim dos Santos <cristiano.joaquim@gmail.com>
2018-11-02 19:52:16 -0300

    Inclusão do arquivo Leaiame.txt e a Carga da Dimensão de Tempo para o Data Mart de Vendas 2

---
+++ scripts/3.1 - Carga Dimensão Tempo.sql
@@ -0,0 +1,33 @@
+USE DMVenda2
+GO
+
+DECLARE @DataIni DATE = '2015-01-01'
+DECLARE @DataFim DATE = '2019-12-31'
+
+DECLARE @DataCurrent DATE
+
+DECLARE @cont INT = 0
+DECLARE @SKTempo INT = 0
+DECLARE @Data DATE
+DECLARE @Dia INT = 0
+DECLARE @Mes INT = 0
+DECLARE @Ano INT = 0
+
+BEGIN
+	SET @DataCurrent = @DataIni
+
+	WHILE @DataCurrent <= @DataFim
+	BEGIN
+		SET @Data = @DataCurrent
+		SET @SKTempo = CONVERT(INT, REPLACE(@Data, '-', ''))
+		SET @Ano = YEAR(@Data)
+		SET @Mes = MONTH(@Data)
+		SET @Dia = DAY(@Data)
+
+		INSERT INTO dbo.DimTempo (SKTempo, Data, Dia, Mes, Ano)
+		VALUES (@SKTempo, @Data, @Dia, @Mes, @Ano)
+
+		SET @cont += 1
+		SET @DataCurrent = DATEADD(DAY, @cont, @DataIni)
+	END
+END
