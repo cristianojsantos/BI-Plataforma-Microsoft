7137f6ebff6995f518a1298f926bf54947232f75
Cristiano Joaquim dos Santos <cristiano.joaquim@gmail.com>
2019-06-16 20:14:05 -0300

    Tratamento do valor de compra do Produto

---
+++ scripts/1.1 - Inclusão do campo Valor de Compra do Produto.sql
@@ -0,0 +1,40 @@
+USE DBVendas
+GO
+
+BEGIN
+    DECLARE cProduto
+	CURSOR FOR
+	SELECT P.cod_produto,
+		   P.valor_produto
+	  FROM dbo.Produto P WITH(NOLOCK)
+
+	DECLARE @cod_produto INT = 0
+	DECLARE @valor_produto FLOAT = 0
+
+	/* 
+	   Como é um exemplo, dexarei o percentual de lucro
+	   fixado em 60% do Produto. Logo, o lucro da venda será este de 60%
+	*/
+	DECLARE @perc_lucro FLOAT = 60
+	DECLARE @valor_compra FLOAT = 0
+
+	OPEN cProduto
+	FETCH NEXT FROM cProduto INTO @cod_produto, @valor_produto
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		SET @valor_compra = @valor_produto - ((@perc_lucro / 100) * @valor_produto)
+
+		PRINT 'Valor do Produto: ' + CONVERT(VARCHAR, @valor_produto)
+		PRINT 'Valor de Compra: ' + CONVERT(VARCHAR, @valor_compra)
+
+		UPDATE dbo.Produto
+		   SET valor_compra = @valor_compra
+		 WHERE cod_produto = @cod_produto
+		
+		FETCH NEXT FROM cProduto INTO @cod_produto, @valor_produto
+	END
+
+	CLOSE cProduto
+	DEALLOCATE cProduto
+END
